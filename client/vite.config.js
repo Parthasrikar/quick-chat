e59cf244523859f2e19f7c29b19bd5920a7e0391
Partha <parthasrikar853@gmail.com>
2025-06-19 12:13:44 +0530

    added preview

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindScrollbar from 'tailwind-scrollbar'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true, // Allow external access
     port: 5173, // Change if needed
@@ -14,5 +14,10 @@ export default defineConfig({
     hmr: {
       host: "rnatm-2409-4070-4ec9-859f-9c53-ae2-8fda-e9ed.a.free.pinggy.link", // Allow HMR on the external host
     },
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: 'all' // This fixes the blocked host error
   }
 })
